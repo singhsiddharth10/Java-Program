f472abf80c5e151ed71b546e68d3b8d6531f17f0
siddharth singh <siddharthsingh@siddharths-MacBook-Air.local>
2024-12-15 23:45:41 +0530

    Second Largest

---
+++ SecondLargest.java
@@ -0,0 +1,30 @@
+import java.util.* ;
+import java.io.*; 
+public class SecondLargest {
+	public static int findSecondLargest(int n, int[] arr) {
+		int mx1,mx2;
+		if(arr[0] > arr[1]){
+			mx1 = arr[0];
+			mx2 = arr[1];
+		}
+		else{
+			mx1 = arr[1];
+			mx2 = arr[0];
+		}
+        
+		for(int i = 2; i < arr.length; i++){
+			if(mx1 < arr[i]){
+				mx2 = mx1;
+				mx1 = arr[i];
+			}
+			if(mx2 < arr[i] && mx1 > arr[i]){
+				mx2 = arr[i];
+			}
+
+		}
+		
+		if(mx1 == mx2) return -1;
+		
+		return mx2;
+	}
+}
